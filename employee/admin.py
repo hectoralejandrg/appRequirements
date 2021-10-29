17c76ac4c16741088da568bc3ead138030560573
AdonisRugel <86493238+AdonisRugel@users.noreply.github.com>
2021-10-28 20:40:47 -0500

    Se crean los modelos de Holidays, Requeriments, Reason

--- employee/admin.py
+++ employee/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from employee.models import Employee
+from employee.models import Employee,Requirements,Reason,Holidays
 
 admin.site.register(Employee)
+admin.site.register(Requirements)
+admin.site.register(Reason)
+admin.site.register(Holidays)
 
--- employee/models.py
+++ employee/models.py
@@ -13,3 +13,34 @@ class Employee(models.Model):
 
     def __str__(self):
         return f'{self.identification} - {self.name} {self.lastname}'
+
+class Requirements(models.Model):
+    code = models.CharField(max_length=15)
+    date_requirement = models.DateField()
+    date_start = models.DateTimeField()
+    date_end = models.DateTimeField()
+    hours_discount = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.code} - {self.date_requirement} - {self.date_star} - {self.date_end} - {self.hours_discount}'
+
+class Reason(models.Model):
+    description = models.CharField(max_length=100)
+    created = models.DateField()
+    updated = models.DateField()
+    state = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'{self.description} - {self.created} - {self.updated}'
+
+class Holidays(models.Model):
+    date_start = models.DateField()
+    date_end = models.DateField()
+    days = models.DateField()
+
+    def __str__(self):
+        return f'{self.date_star} - {self.date_end} - {self.days}'
+
+
+
+
