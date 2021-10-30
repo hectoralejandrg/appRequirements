552fcac7092fe7a09efd0969a9facac8af7de2cd
AdonisRugel <86493238+AdonisRugel@users.noreply.github.com>
2021-10-29 20:40:44 -0500

    Listado de requirements

--- employee/urls.py
+++ employee/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from employee.views import EmployeeGenericView
+from employee.views import EmployeeGenericView,RequirementsGenericView
 
 app_name= 'employee'
 
 urlpatterns =[
     path('employee/', EmployeeGenericView.as_view(), name="list_employee"),
+    path('requirements/', RequirementsGenericView.as_view(), name="list_requirements"),
 ]
+
--- employee/views.py
+++ employee/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 
-from employee.models import Employee
+from employee.models import Employee, Requirements
 
 
 class EmployeeGenericView(ListView):
     model = Employee
     context_object_name = 'employees'
+
+class RequirementsGenericView(ListView):
+    model = Requirements
+    context_object_name = 'requirements'
 
